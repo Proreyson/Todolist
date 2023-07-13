ade3e539759fb216a858d833de6e96e342a60e0a
Proreyson <r3ys0ncarpio14@gmail.com>
2023-07-14 01:15:06 +0800

    Todolist

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    public function index(){
+        $todos = Todo::orderBy('created_at', 'desc')->get();
+        return view('welcome')->with('todos', $todos);
+    }
+    public function create(Request $request){
+
+        $todo = new Todo();
+        $todo->content = $request->content;
+        $todo->status = "pending";
+
+        $todo->save();
+
+
+        return redirect()->route('home')->with('success', "Todo added successfully");
+    }
+
+    public function update($id) {
+
+        $todo = Todo::find($id);
+        $todo->status = "complete";
+        $todo->save();
+
+        return redirect()->route('home')->with('success', "Todo marked as complete!");
+    }
+
+    public function delete($id) {
+        $todo = Todo::find($id);
+        $todo->status = "deleted";
+        $todo->delete();
+
+        return redirect()->route('home')->with('success', "Todo deleted!");
+    }   
+ }
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [TodoController::class, 'index'])->name('home');
+
+
+
+Route::post('/createtodo', [TodoController::class, 'create'])->name('createtodo');
+
+Route::get('/updatetodo/{id}', [TodoController::class, 'update'])->name('updatetodo');
+
+Route::get('/deletetodo/{id}', [TodoController::class, 'delete'])->name('deletetodo');
